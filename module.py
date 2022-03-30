2386f804c79156b658b859a6e8a3388f7d901a37
Muhammad Ahmad <ahmadfsbd@gmail.com>
2022-03-30 19:00:29 +0500

    more

--- module.py
+++ module.py
@@ -1,21 +1,13 @@
-# Example python module that contains a class
+# Example python module that contains two classes
 
 class math:
-    # Class varibles
+    # Class varibles are common for all the instances of class
+    # They can be refrenced inside class methods if self is passed
     name = "Math class"
     operation = "Basic addition, subtraction, multiplication and division"
 
-
-    # Static methods (self is not passed)
-    @staticmethod 
-    def add(a, b):
-        add = a + b
-        return add
-    
-    @staticmethod
-    def sub(a, b):
-        sub = a - b
-        return sub
+    # A method has the first argument (self) as the object to which it is bound.
+    # Python automatically passes the bound object to the method as the first argument. By convention, its name is self.
 
     # Class methods (self is passed)
     def describe(self):
@@ -28,7 +20,25 @@ class math:
     def divide(self, a, b):
         return a / b
 
+    # Static methods : (self is not passed) are not bound to class objects
+    # it means that they cannot access or change object's state
+    # static method example
+    @staticmethod
+    def add(a, b):
+        add = a + b
+        return add
+
+    @staticmethod
+    def sub(a, b):
+        sub = a - b
+        return sub
+
+
+
 class employee:
+    # __init__ is used to initialize private attributes/variables when creating an instance from class
+    # These attributes are specific to the object and cannot be used across class objects
+    # To initialize the class object >> employee(name, age) will be used
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -36,4 +46,3 @@ class employee:
     def weekly_salary(self, hourly_salary, hours_worked):
         weekly_sal = hourly_salary * hours_worked
         print("Employee {} is {} years old. \n His weekly salary is : {}".format(self.name, self.age, weekly_sal))
-
